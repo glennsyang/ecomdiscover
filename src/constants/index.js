1441ca1570633567cab016eee1f881a278ef975b
Glenn Sheppard <gsheppard.yang@gmail.com>
2020-04-28 22:48:32 -0700

    finalize rich text editor

--- src/constants/index.js
+++ src/constants/index.js
@@ -52,3 +52,33 @@ export const customStyles = {
     }),
     placeholder: styles => ({ ...styles, color: '#cbd5e0' }),
 }
+
+/* 
+ * Quill modules to attach to editor
+ * See https://quilljs.com/docs/modules/ for complete options
+ */
+export const editorModules = {
+    toolbar: [
+        [{ 'font': [] }],
+        [{ size: [] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' },
+        { 'indent': '-1' }, { 'indent': '+1' }],
+        ['link'],
+        ['clean']
+    ],
+    clipboard: {
+        // toggle to add extra line breaks when pasting HTML:
+        matchVisual: false,
+    }
+}
+/* 
+ * Quill editor formats
+ * See https://quilljs.com/docs/formats/
+ */
+export const editorFormats = [
+    'header', 'font', 'size',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image', 'video'
+]
