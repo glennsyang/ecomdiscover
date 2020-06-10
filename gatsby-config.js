04df46838935a098620b75cfb122fbdcb9eea963
Glenn Sheppard <gsheppard.yang@gmail.com>
2020-06-10 09:29:15 -0700

    add firebase env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,7 +61,19 @@ module.exports = {
     {
       resolve: 'gatsby-source-firestore',
       options: {
-        credential: require("./firebase.json"),
+        //credential: require("./firebase.json"),
+        credential: {
+          type: process.env.GATSBY_FIREBASE_TYPE,
+          project_id: process.env.GATSBY_FIREBASE_PROJECT_ID,
+          private_key_id: process.env.GATSBY_FIREBASE_PRIVATE_KEY_ID,
+          private_key: process.env.GATSBY_FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+          client_email: process.env.GATSBY_FIREBASE_CLIENT_EMAIL,
+          client_id: process.env.GATSBY_FIREBASE_CLIENT_ID,
+          auth_uri: process.env.GATSBY_FIREBASE_AUTH_URI,
+          token_uri: process.env.GATSBY_FIREBASE_TOKEN_URI,
+          auth_provider_x509_cert_url: process.env.GATSBY_FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+          client_x509_cert_url: process.env.GATSBY_FIREBASE_CLIENT_X509_CERT_URL,
+        },
         types: [
           {
             type: 'Categories',
@@ -156,7 +168,6 @@ module.exports = {
           authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
           databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
           projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
-          //projectId: activeProjectId,
           storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
           messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
           appId: process.env.GATSBY_FIREBASE_APP_ID,
