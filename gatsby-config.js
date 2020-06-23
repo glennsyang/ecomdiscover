562a62ac8763aa8e693c8cc91d275bb77db47b89
Glenn Sheppard <gsheppard.yang@gmail.com>
2020-06-23 10:45:55 -0700

    add Google SignIn, Fix ad spacing for Submit Review page, When unpublishing review, remove from review count

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,6 +66,7 @@ module.exports = {
           type: process.env.GATSBY_FIREBASE_TYPE,
           project_id: process.env.GATSBY_FIREBASE_PROJECT_ID,
           private_key_id: process.env.GATSBY_FIREBASE_PRIVATE_KEY_ID,
+          //private_key: process.env.GATSBY_FIREBASE_PRIVATE_KEY,
           private_key: process.env.GATSBY_FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
           client_email: process.env.GATSBY_FIREBASE_CLIENT_EMAIL,
           client_id: process.env.GATSBY_FIREBASE_CLIENT_ID,
